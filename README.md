43651f3f9e197bfa74063b308ebfef634ff67859
Aaron <aaron@kloudless.com>
2015-05-06 01:33:15 -0700

    addition of search by user
    removing pdb line
    
    updatign readme
    
    fixing readme
    
    readme
    
    readme additions
    
    add todo and author
    
    fixing usage
    
    more readme
    
    more
    
    more
    
    more

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Social-Analytics
+
+A Django application that:
+* let's you search for a keyword in any number of tweets and displays an analysis of their sentiments.
+* let's you search through any number of tweets of a specific user and displays an analysis of their sentiments.
+
+## Usage
+### On Heroku
+* https://protected-earth-8489.herokuapp.com/
+
+### Run Locally
+1. Clone this repository.
+2. Install all [requirements.txt](https://github.com/aaaronlopez/Social-Analytics/blob/master/requirements.txt).
+3. Obtain a Twitter Consumer Key (API Key), Consumer Secret (API Secret), Access Token, and Access Token Secret from [Twitter Developers](https://dev.twitter.com/).
+4. Go to [config-example.yml](https://github.com/aaaronlopez/Social-Analytics/blob/master/config-example.yml) and change `secret_key`, `consumer_key`, `consumer_secret`, `access_token`, `access_token_secret`. Rename this to config.yml.
+	* `secret_key` can be any random 50 character string, use: `django.utils.crypto.get_random_string(50, chars)`
+	* You can also just change the secret key variables at the top of [local.py](https://github.com/aaaronlopez/Social-Analytics/blob/master/project/settings/local.py) if you don't want to use a .yml.
+6. Run `python manage.py runserver` from the base of this repo. (this will not work unless you do the above steps)
+
+## How it Works
+* Uses [Django](https://www.djangoproject.com/) to run on the web.
+* Connects to the [Twitter](https://twitter.com/) API using the power of [Tweepy](https://github.com/tweepy/tweepy)!
+* Sends the data from [Tweepy](https://github.com/tweepy/tweepy) to a rich dashboard that utilizes awesome [Bootstrap](http://getbootstrap.com/), [Google Charts](https://developers.google.com/chart/), and more!
+
+## To-Do
+* Add the ability to search through more social media!
+* Add more visually stimulating data representation!
+
+## Author
+Aaron Lopez, UC Berkeley
+
+## Acknowledgements
+* [Django](https://www.djangoproject.com/)
+* [Heroku](https://www.heroku.com/)
+* [Twitter](https://twitter.com/)
+* [Tweepy](https://github.com/tweepy/tweepy)
+* [Bootstrap](http://getbootstrap.com/)
+* [Google Charts](https://developers.google.com/chart/)
+* [Bootstrap-Table](https://github.com/wenzhixin/bootstrap-table)
+* [Virtualenv](https://virtualenv.pypa.io/en/latest/)
+
--- social_analytics/views.py
+++ social_analytics/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
 
-from tweepy import OAuthHandler, Stream
+from tweepy import OAuthHandler, Stream, API
 from tweepy.streaming import StreamListener
 from textblob import TextBlob
 from collections import Counter
-# from scipy.stats import mode
 from numpy import median, average, std
 import json
 
@@ -20,14 +19,13 @@ def index(request):
 
 def analyze_tweets(request):
 	selection = request.POST.get('key-selector')
-	import pdb; pdb.set_trace()
 	if selection == "by Keyword":
 		return tweets_by_key(request)
 	elif selection == "by User":
 		return tweets_by_user(request)
 
 def tweets_by_key(request):
-	keyword = request.POST.get('keyword')
+	keyword = request.POST.get('key')
 	tweet_limit = int(request.POST.get('limit'))
 
 	histogram_data = [['Sentiment']]
@@ -37,8 +35,9 @@ def tweets_by_key(request):
 				['Negative', 0]]
 	statistics = {}
 	tweets = []
-	tweet_count = 0
+	# tweet_count = 0
 
+	#Twitter Stream
 	class listener(StreamListener):
 		def __init__(self, api=None):
 			super(listener, self).__init__()
@@ -75,7 +74,7 @@ def tweets_by_key(request):
 				elif sentiment < 0:
 					pie_chart[3][1] += 1
 				self.num_tweets += 1
-				tweet_count += 1
+				# tweet_count += 1
 				return True		
 			else:
 				return False
@@ -102,5 +101,65 @@ def tweets_by_key(request):
 		'min_tweet_id': min(statistics, key=statistics.get),
 		'max_tweet_id': max(statistics, key=statistics.get),
 	}
-	return render(request, 'dashboard.html', data)
+	return render(request, 'dashboard-keyword.html', data)
+
+def tweets_by_user(request):
+	user = request.POST.get('key')
+	tweet_limit = int(request.POST.get('limit'))
+
+	histogram_data = [['Sentiment']]
+	pie_chart = [['Sentiment', 'Value'], 
+				['Neutral', 0],
+				['Positive', 0],
+				['Negative', 0]]
+	statistics = {}
+	tweets = []
+	# tweet_count = 0
+
+	#Twitter Data
+	api = API(auth)
+	tweet_data = api.user_timeline(screen_name = user, count=tweet_limit)
+	for tweet in tweet_data:
+		text = tweet.text
+		id = tweet.id
+		blob = TextBlob(text)
+		sentiment = blob.sentiment.polarity
+
+		#Histogram
+		histogram_data.append([blob.sentiment.polarity])
+
+		#Statistics
+		statistics[id] = sentiment
+		
+		#Table
+		tweets.append({
+			'sentiment': float("{0:.2f}".format(sentiment)),
+			'text': text + ' <a href="https://twitter.com/statuses/' + 
+							str(tweet.id) + '"target="_blank">View</a>'
+			})
+
+		#Piechart
+		if sentiment == 0:
+			pie_chart[1][1] += 1
+		elif sentiment > 0:
+			pie_chart[2][1] += 1
+		elif sentiment < 0:
+			pie_chart[3][1] += 1
+
+	#Context Data
+	data = {
+		'user': user,
+		'limit': tweet_limit,
+		'histogram_data': histogram_data,
+		'pie_chart': pie_chart,
+		'tweets': json.dumps(tweets),
+		'average': float("{0:.5f}".format(average(statistics.values()))),
+		'median': float("{0:.5f}".format(median(statistics.values()))),
+		'std': float("{0:.5f}".format(std(statistics.values()))),
+		'min': float("{0:.5f}".format(min(statistics.values()))),
+		'max': float("{0:.5f}".format(max(statistics.values()))),
+		'min_tweet_id': min(statistics, key=statistics.get),
+		'max_tweet_id': max(statistics, key=statistics.get),
+	}
+	return render(request, 'dashboard-user.html', data)
 
